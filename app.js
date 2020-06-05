92445b7305dfd7e1c4b0a3f1f156fd180136511b
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-06-05 11:21:17 -0400

    added event listener to app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const square = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+
+    
+})
